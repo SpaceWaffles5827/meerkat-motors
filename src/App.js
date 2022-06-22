5cff620a5734e31bdc8d5c632ec2d0eb762b00cb
unknown <jackhartwig7@gmail.com>
2022-06-22 16:01:07 -0500

    added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="bg-image">
-        <div className='NavBar'>
+      <div className='navBar'>
+        <img className='logo' src='/logo.png' height='100px'></img>
+        <div className='textBox'>
           <h1>Meerkat Motors</h1>
+          <h1>2503 Newby Rd SW</h1>
+          <h1>(256)-665-1645</h1>
+        </div>
+      </div>
+      <div className='pageContnet'>
+        <div className='centerBoxBottom'>
+          <div className='centerBoxTop'>
+            <h1>WE BUY</h1>
+            <h1>USED CARS</h1>
+          </div>
         </div>
       </div>
     </div>
